578e12c84bc9280226a1e05864f30e2062efec96
Daniel Cooper <z5418233@ad.unsw.edu.au>
2023-09-25 10:32:06 +1000

    Added user privesc

--- jamespc/totalwc.sh
+++ jamespc/totalwc.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-if [ $# -lt 1 ]; then
-    echo "usage: $0 <dir>"
-    exit 1
-fi
-
-start_dir=$(pwd)
-dirname=$1
-
-cd $dirname
 wc $(ls) | tail -n 1 | sed -E 's/^ +//' | cut -d' ' -f 1
-cd $start_dir
